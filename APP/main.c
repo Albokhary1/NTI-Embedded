309dcf257faa8a8606b1ff3f08f471e4795849f2
Abdelrahman Elbokhary <abdelrahmanalbokhary72@gmail.com>
2026-09-12 00:27:18 +0300

    Keypad&7seg

--- APP/main.c
+++ APP/main.c
@@ -4,19 +4,15 @@
  *  Author: Abdelrahman Elbokhary
  */
 
-#include "DIO.h"
-#include "LCD.h"
-#include "APP_lcd_name.h"
+#include "APP_Keypad_7Seg.h"
 
 int main(void)
 {
-    LCD_Init();
-    
-    APP_DisplayName();
+    APP_Keypad7Seg_Init();
 
     while (1)
     {
-        
+        APP_Keypad7Seg_Task();
     }
     
     return 0;
